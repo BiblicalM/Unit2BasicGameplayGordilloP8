a28bd8becd5cd6a6386054e665ce75249e830c8a
Maximiliano Gordillo <724910@student.dusd.net>
2024-03-27 15:28:08 -0700

    FINISHED

--- Challenge2Gordillo/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Challenge2Gordillo/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,14 +6,27 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    bool canSpawnDog = true;
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && canSpawnDog)
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            SpawnDog();
+            canSpawnDog = false;
+            Invoke("CoolDown", 1);
         }
+    }
+
+    void SpawnDog()
+    {
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+    }
+
+    void CoolDown()
+    {
+        canSpawnDog = true;
     }
 }
